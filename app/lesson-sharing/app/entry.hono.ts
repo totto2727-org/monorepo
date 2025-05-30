49d0c455fe93394697a8cfb9a44705e2d54d7326
totto2727 <h_tsuchida@yumemi.co.jp>
2025-05-31 00:35:27 +0900

    feat: integrate context storage and update middleware to use getContext in lesson-sharing app

--- app/lesson-sharing/app/entry.hono.ts
+++ app/lesson-sharing/app/entry.hono.ts
@@ -2,17 +2,20 @@ import { clerkMiddleware } from "@hono/clerk-auth"
 import { Effect } from "@totto/function/effect"
 import { Hono } from "hono"
 import { env } from "hono/adapter"
+import { contextStorage } from "hono/context-storage"
 import { logger } from "hono/logger"
-import type { Env } from "./types/hono"
+import { getContext } from "#@/feature/hono.js"
+import type { Env } from "#@/types/hono.js"
 
 export const appEffect = Effect.gen(function* () {
   return new Hono<Env>()
+    .use(contextStorage())
     .use(logger())
-    .use("/api", (c, next) =>
+    .use("/api", (...args) =>
       clerkMiddleware({
-        publishableKey: env(c).VITE_CLERK_PUBLISHABLE_KEY,
-        secretKey: env(c).CLERK_SECRET_KEY,
-      })(c, next),
+        publishableKey: env(getContext()).VITE_CLERK_PUBLISHABLE_KEY,
+        secretKey: env(getContext()).CLERK_SECRET_KEY,
+      })(...args),
     )
     .get("/api/hello", (c) => {
       return c.json({
