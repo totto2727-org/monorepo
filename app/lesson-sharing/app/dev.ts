722d6a851bc50173ac8eafd69cd2c7b7fcc50a5b
totto2727 <h_tsuchida@yumemi.co.jp>
2025-07-04 00:34:38 +0900

    feat: integrate authentication middlewares into app and entry files for lesson-sharing app

--- app/lesson-sharing/app/dev.ts
+++ app/lesson-sharing/app/dev.ts
@@ -1,11 +1,16 @@
 import { Effect, Logger } from "@totto/function/effect"
-import { appEffect, clerkCrenditionalLive } from "#@/entry.hono.js"
+import { appEffect } from "#@/entry.hono.js"
+import {
+  clerkAuthMiddlewaresLive,
+  clerkCrenditionalLive,
+} from "./feature/auth/clerk"
 import { drizzleClientLive } from "./feature/db/drizzle"
 import { inMemorySQLiteClientLive } from "./feature/db/sqlite"
 
 const app = await Effect.gen(function* () {
   return yield* appEffect
 }).pipe(
+  Effect.provide(clerkAuthMiddlewaresLive),
   Effect.provide(clerkCrenditionalLive),
   Effect.provide(drizzleClientLive),
   Effect.provide(inMemorySQLiteClientLive),
--- app/lesson-sharing/app/entry.hono.ts
+++ app/lesson-sharing/app/entry.hono.ts
@@ -1,22 +1,17 @@
-import { clerkMiddleware } from "@hono/clerk-auth"
 import { Effect } from "@totto/function/effect"
 import { Hono } from "hono"
-import { env } from "hono/adapter"
 import { contextStorage } from "hono/context-storage"
 import { logger } from "hono/logger"
-import { getContext } from "#@/feature/hono.js"
 import type { Env } from "#@/types/hono.js"
+import { AuthMiddlewares } from "./feature/auth"
 
 export const appEffect = Effect.gen(function* () {
+  const authMiddlewares = yield* AuthMiddlewares
+
   return new Hono<Env>()
     .use(contextStorage())
     .use(logger())
-    .use("/api", (...args) =>
-      clerkMiddleware({
-        publishableKey: env(getContext()).VITE_CLERK_PUBLISHABLE_KEY,
-        secretKey: env(getContext()).CLERK_SECRET_KEY,
-      })(...args),
-    )
+    .use("/api", authMiddlewares.baseMiddleware)
     .get("/api/hello", (c) => {
       return c.json({
         message: "Hello",
