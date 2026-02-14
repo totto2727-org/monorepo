6a94818b1a070e0fcbd6d15e17005695b60901f9
totto2727 <kaihatu.totto2727@gmail.com>
2026-02-14 15:57:25 +0900

    feat: add bun-plugin-react-compiler package

---
+++ js/package/bun-plugin-react-compiler/plugin.ts
@@ -0,0 +1,32 @@
+// https://github.com/oven-sh/bun/issues/24356#issuecomment-3877275486
+import * as babel from '@babel/core'
+import { Predicate } from '@package/function/effect'
+import BabelPluginReactCompiler from 'babel-plugin-react-compiler'
+
+const options = {}
+
+const reactCompiler: Bun.BunPlugin = {
+  name: 'react-compiler',
+  setup({ onLoad }) {
+    onLoad({ filter: /\.[jt]sx$/ }, async (args) => {
+      const input = await Bun.file(args.path).text()
+      const result = await babel.transformAsync(input, {
+        ast: false,
+        babelrc: false,
+        configFile: false,
+        filename: args.path,
+        parserOpts: { plugins: ['jsx', 'typescript'] },
+        plugins: [[BabelPluginReactCompiler, options]],
+        sourceMaps: Bun.env.NODE_ENV === 'development',
+      })
+
+      if (Predicate.isNullable(result?.code)) {
+        throw new Error(`Failed to compile ${args.path}`)
+      }
+
+      return { contents: result.code, loader: 'tsx' }
+    })
+  },
+}
+
+export default reactCompiler
