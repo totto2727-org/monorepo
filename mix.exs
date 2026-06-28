1f02beb49a959f5693729132e10f3ed55bf28ae4
totto2727 <kaihatu.totto2727@gmail.com>
2026-06-28 11:28:13 +0900

    refactor(elixir): flatten umbrella structure and move root to repo root

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule Monorepo.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      name: "Monorepo",
+      apps_path: "elixir",
+      version: "0.1.0",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    []
+  end
+end
